6f849537e4772e51a3db1495f34f402cb5f2000c
Koen Verburg <creativekoen@gmail.com>
2024-09-13 13:29:07 +0200

    little clean up

--- apps/main/index.ts
+++ apps/main/index.ts
@@ -1,9 +1,3 @@
 import { common } from '@toolbox/common'
-import { Configuration, FooService } from "./src/registery";
 
-function main() {
-  const config = Configuration
-  common()
-}
-
-main()
+common()
